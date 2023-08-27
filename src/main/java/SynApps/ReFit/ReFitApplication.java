e22403c111cc84379f6f426e89bf1077885a5b97
Brian <bwook9908@gmail.com>
2023-08-27 17:41:15 +0900

    chore: first init

---
+++ src/main/java/SynApps/ReFit/ReFitApplication.java
@@ -0,0 +1,13 @@
+package SynApps.ReFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SynApps/ReFit/ReFitApplicationTests.java
@@ -0,0 +1,13 @@
+package SynApps.ReFit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
