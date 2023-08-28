494fa49bee53a051d23517271b491ac6e2c8542e
Brian <bwook9908@gmail.com>
2023-08-28 11:07:59 +0900

    testing

--- src/main/java/SynApps/ReFit/controller/testController.java
+++ src/main/java/SynApps/ReFit/controller/testController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class testController {
     @GetMapping("hello")
     public String hello(){
-        return "hello!!";
+        return "hello!! this is refit test!";
     }
     //testing
 }
