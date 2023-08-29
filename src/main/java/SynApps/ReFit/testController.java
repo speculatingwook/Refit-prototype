cf9275dc14acfa8a62eee613e54948ebb8c38a3f
Brian <bwook9908@gmail.com>
2023-08-29 11:26:14 +0900

    feat: swagger config complete

--- src/main/java/SynApps/ReFit/testController.java
+++ src/main/java/SynApps/ReFit/testController.java
@@ -1,4 +1,4 @@
-package SynApps.ReFit.controller;
+package SynApps.ReFit;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
